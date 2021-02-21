ef399bdb7b51ee2b0715723d83c90b00230458ae
PresentKim <debe3721@gmail.com>
2021-02-21 17:45:48 +0900

    Init KorTimings plugin

---
+++ src/kim/present/kortimings/Loader.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace kim\present\kortimings;
+
+use pocketmine\plugin\PluginBase;
+
+final class Loader extends PluginBase{
+    protected function onLoad() : void{
+    }
+}
